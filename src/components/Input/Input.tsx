5f641e4cd3d9eca74b50a68bf645a9b6ccdea874
MarcelloAragoni <32502447+MarcelloAragoni@users.noreply.github.com>
2022-09-17 15:28:39 -0300

    created input component (#2)
    Trello: https://trello.com/c/Iav0wXku/8-todo-list
    
    Description
    Created input component.
    
    Screenshot
    ![image](https://user-images.githubusercontent.com/32502447/189733541-02f48297-96a5-408b-8f9a-c662f454d4d2.png)

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,39 @@
+import cn from "classnames";
+
+import "./input.scss";
+
+type Props = {
+  placeholder: string;
+  name: string;
+  value: string;
+  onChange: (value: string) => void;
+  variant: string;
+};
+
+export default function Input({
+  placeholder,
+  name,
+  value,
+  onChange,
+  variant,
+}: Props) {
+  const inputStyle = cn(
+    "input",
+    {"input--dark": variant === "dark"},
+    {"input--light": variant === "light"}
+  );
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    onChange(event.target.value);
+  }
+
+  return (
+    <input
+      onChange={handleChange}
+      name={name}
+      className={inputStyle}
+      placeholder={placeholder}
+      value={value}
+    />
+  );
+}
